aed62bc68045476a6aaf8ea00f56330907335ddb
AI Invigilate <GoodnessThembaC@gmail.com>
2025-06-23 12:44:52 +0200

    static files configure -not working

--- mzansiplug/settings.py
+++ mzansiplug/settings.py
@@ -191,6 +191,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+# STATIC_URL = 'static/'
+# MEDIA_URL = '/media/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Fetch environment variables for AWS S3
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -205,29 +216,37 @@ AWS_S3_READY = (
     and AWS_S3_REGION_NAME is not None
 )
 
+print(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
+
 if AWS_S3_READY:
-    # Configure AWS S3 settings
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Static files (CSS, JavaScript, images)
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    # Media files (uploads)
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    # Optional: Cache control settings
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
 else:
-    STATIC_URL = 'static/'
+    STATIC_URL = '/static/'
     MEDIA_URL = '/media/'
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static'
-    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+print("STATICFILES_STORAGE:", STATICFILES_STORAGE)
+print("DEFAULT_FILE_STORAGE:", DEFAULT_FILE_STORAGE)
+
+
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
